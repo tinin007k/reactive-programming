84664ebc1ac424fa6e968532aad17e5c2ece205f
hemant-kumar.singh@hpe.com <hemant-kumar.singh@hpe.com>
2023-07-28 15:00:17 +0530

    first commit. added the pom.xml and first test class

---
+++ src/main/java/FirstReactiveProgram.java
@@ -0,0 +1,19 @@
+import reactor.core.publisher.Mono;
+
+public class FirstReactiveProgram {
+
+    public Mono<String> fruitMono() {
+        return Mono.just("Mango").log();
+    }
+
+    public static void main(String[] args) {
+
+        FirstReactiveProgram fluxAndMonoServices
+                = new FirstReactiveProgram();
+
+        fluxAndMonoServices.fruitMono()
+                .subscribe(s -> {
+                    System.out.println("Mono -> s = " + s);
+                });
+    }
+}
